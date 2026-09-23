cf49db33db9bc75b0115ef241ca9656ff3adf18d
Ayden250 <suarez.ayden@student.ccm.edu>
2026-09-23 11:09:38 -0400

    Update temperature_category.py

---
+++ temperature_category.py
@@ -0,0 +1,15 @@
+#Ayden Suarez
+#CCP-131
+#Week 5
+#Lab 1
+#Temperature 
+#09/30/26
+temp= float(input("(Enter a temperature: "))
+if(temp>=80):
+   print("Hot")
+elif(temp<=79):
+    print("Warm")
+elif(temp>=50):
+    print("Warm")
+elif(temp<=49.9):
+    print("Cold")
